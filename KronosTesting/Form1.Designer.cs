3e477762e70f677ff0d308e6bbcfaa0eed0113e5
jchavis <jchavis@DBA>
2017-08-16 14:55:18 -0700

    Add project files.

---
+++ KronosTesting/Form1.Designer.cs
@@ -0,0 +1,323 @@
+﻿using System.Data.SqlClient;
+
+namespace KronosTesting
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.btnName = new System.Windows.Forms.Button();
+            this.cbSchools = new System.Windows.Forms.ComboBox();
+            this.lblSchools = new System.Windows.Forms.Label();
+            this.tbEmail = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.tbPhone = new System.Windows.Forms.TextBox();
+            this.lblEmail = new System.Windows.Forms.Label();
+            this.lblfName = new System.Windows.Forms.Label();
+            this.tbfName = new System.Windows.Forms.TextBox();
+            this.lbllName = new System.Windows.Forms.Label();
+            this.tblName = new System.Windows.Forms.TextBox();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.button2 = new System.Windows.Forms.Button();
+            this.lblRecordCount = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.dgData = new System.Windows.Forms.DataGridView();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.tbLast4 = new System.Windows.Forms.TextBox();
+            this.lblLast4 = new System.Windows.Forms.Label();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.panel4 = new System.Windows.Forms.Panel();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgData)).BeginInit();
+            this.panel2.SuspendLayout();
+            this.panel3.SuspendLayout();
+            this.panel4.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // btnName
+            // 
+            this.btnName.Location = new System.Drawing.Point(0, 9);
+            this.btnName.Name = "btnName";
+            this.btnName.Size = new System.Drawing.Size(90, 21);
+            this.btnName.TabIndex = 0;
+            this.btnName.Text = "Clear Fliters";
+            this.btnName.UseVisualStyleBackColor = true;
+            this.btnName.Click += new System.EventHandler(this.btnName_Click);
+            // 
+            // cbSchools
+            // 
+            this.cbSchools.FormattingEnabled = true;
+            this.cbSchools.Location = new System.Drawing.Point(154, 9);
+            this.cbSchools.Name = "cbSchools";
+            this.cbSchools.Size = new System.Drawing.Size(137, 21);
+            this.cbSchools.TabIndex = 2;
+            this.cbSchools.SelectedIndexChanged += new System.EventHandler(this.cbSchools_SelectedIndexChanged);
+            // 
+            // lblSchools
+            // 
+            this.lblSchools.AutoSize = true;
+            this.lblSchools.Location = new System.Drawing.Point(113, 13);
+            this.lblSchools.Name = "lblSchools";
+            this.lblSchools.Size = new System.Drawing.Size(40, 13);
+            this.lblSchools.TabIndex = 3;
+            this.lblSchools.Text = "School";
+            // 
+            // tbEmail
+            // 
+            this.tbEmail.Location = new System.Drawing.Point(956, 9);
+            this.tbEmail.Name = "tbEmail";
+            this.tbEmail.Size = new System.Drawing.Size(137, 20);
+            this.tbEmail.TabIndex = 4;
+            this.tbEmail.TextChanged += new System.EventHandler(this.tbEmail_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(918, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(32, 13);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Email";
+            // 
+            // tbPhone
+            // 
+            this.tbPhone.Location = new System.Drawing.Point(771, 10);
+            this.tbPhone.Name = "tbPhone";
+            this.tbPhone.Size = new System.Drawing.Size(137, 20);
+            this.tbPhone.TabIndex = 6;
+            this.tbPhone.TextChanged += new System.EventHandler(this.tbPhone_TextChanged);
+            // 
+            // lblEmail
+            // 
+            this.lblEmail.AutoSize = true;
+            this.lblEmail.Location = new System.Drawing.Point(727, 13);
+            this.lblEmail.Name = "lblEmail";
+            this.lblEmail.Size = new System.Drawing.Size(38, 13);
+            this.lblEmail.TabIndex = 7;
+            this.lblEmail.Text = "Phone";
+            this.lblEmail.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // lblfName
+            // 
+            this.lblfName.AutoSize = true;
+            this.lblfName.Location = new System.Drawing.Point(306, 13);
+            this.lblfName.Name = "lblfName";
+            this.lblfName.Size = new System.Drawing.Size(57, 13);
+            this.lblfName.TabIndex = 9;
+            this.lblfName.Text = "First Name";
+            // 
+            // tbfName
+            // 
+            this.tbfName.Location = new System.Drawing.Point(369, 9);
+            this.tbfName.Name = "tbfName";
+            this.tbfName.Size = new System.Drawing.Size(137, 20);
+            this.tbfName.TabIndex = 8;
+            this.tbfName.TextChanged += new System.EventHandler(this.tbfName_TextChanged);
+            // 
+            // lbllName
+            // 
+            this.lbllName.AutoSize = true;
+            this.lbllName.Location = new System.Drawing.Point(516, 13);
+            this.lbllName.Name = "lbllName";
+            this.lbllName.Size = new System.Drawing.Size(58, 13);
+            this.lbllName.TabIndex = 11;
+            this.lbllName.Text = "Last Name";
+            // 
+            // tblName
+            // 
+            this.tblName.Location = new System.Drawing.Point(580, 9);
+            this.tblName.Name = "tblName";
+            this.tblName.Size = new System.Drawing.Size(137, 20);
+            this.tblName.TabIndex = 10;
+            this.tblName.TextChanged += new System.EventHandler(this.tblName_TextChanged);
+            // 
+            // panel1
+            // 
+            this.panel1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.panel1.Controls.Add(this.button2);
+            this.panel1.Controls.Add(this.lblRecordCount);
+            this.panel1.Controls.Add(this.button1);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.panel1.Location = new System.Drawing.Point(5, 757);
+            this.panel1.Name = "panel1";
+            this.panel1.Padding = new System.Windows.Forms.Padding(5);
+            this.panel1.Size = new System.Drawing.Size(1269, 30);
+            this.panel1.TabIndex = 12;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(143, 3);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(146, 23);
+            this.button2.TabIndex = 15;
+            this.button2.Text = "Create a Base Lead";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // lblRecordCount
+            // 
+            this.lblRecordCount.AutoSize = true;
+            this.lblRecordCount.Location = new System.Drawing.Point(298, 8);
+            this.lblRecordCount.Name = "lblRecordCount";
+            this.lblRecordCount.Size = new System.Drawing.Size(90, 13);
+            this.lblRecordCount.TabIndex = 13;
+            this.lblRecordCount.Text = "Current Records: ";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(1, 2);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(118, 25);
+            this.button1.TabIndex = 14;
+            this.button1.Text = "Reload";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // dgData
+            // 
+            this.dgData.AllowUserToAddRows = false;
+            this.dgData.AllowUserToDeleteRows = false;
+            this.dgData.AllowUserToOrderColumns = true;
+            this.dgData.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.AllCells;
+            this.dgData.AutoSizeRowsMode = System.Windows.Forms.DataGridViewAutoSizeRowsMode.AllCells;
+            this.dgData.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgData.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.dgData.Location = new System.Drawing.Point(0, 0);
+            this.dgData.Name = "dgData";
+            this.dgData.ReadOnly = true;
+            this.dgData.RowTemplate.Resizable = System.Windows.Forms.DataGridViewTriState.True;
+            this.dgData.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.CellSelect;
+            this.dgData.Size = new System.Drawing.Size(1269, 710);
+            this.dgData.TabIndex = 1;
+            // 
+            // panel2
+            // 
+            this.panel2.Controls.Add(this.tbLast4);
+            this.panel2.Controls.Add(this.lblLast4);
+            this.panel2.Controls.Add(this.tbEmail);
+            this.panel2.Controls.Add(this.btnName);
+            this.panel2.Controls.Add(this.cbSchools);
+            this.panel2.Controls.Add(this.lbllName);
+            this.panel2.Controls.Add(this.lblSchools);
+            this.panel2.Controls.Add(this.tblName);
+            this.panel2.Controls.Add(this.label1);
+            this.panel2.Controls.Add(this.lblfName);
+            this.panel2.Controls.Add(this.tbPhone);
+            this.panel2.Controls.Add(this.tbfName);
+            this.panel2.Controls.Add(this.lblEmail);
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel2.Location = new System.Drawing.Point(5, 5);
+            this.panel2.Name = "panel2";
+            this.panel2.Padding = new System.Windows.Forms.Padding(5);
+            this.panel2.Size = new System.Drawing.Size(1269, 42);
+            this.panel2.TabIndex = 13;
+            // 
+            // tbLast4
+            // 
+            this.tbLast4.Location = new System.Drawing.Point(1142, 10);
+            this.tbLast4.Name = "tbLast4";
+            this.tbLast4.Size = new System.Drawing.Size(100, 20);
+            this.tbLast4.TabIndex = 13;
+            this.tbLast4.TextChanged += new System.EventHandler(this.tbLast4_TextChanged);
+            // 
+            // lblLast4
+            // 
+            this.lblLast4.AutoSize = true;
+            this.lblLast4.Location = new System.Drawing.Point(1103, 12);
+            this.lblLast4.Name = "lblLast4";
+            this.lblLast4.Size = new System.Drawing.Size(33, 13);
+            this.lblLast4.TabIndex = 12;
+            this.lblLast4.Text = "Last4";
+            // 
+            // panel3
+            // 
+            this.panel3.Controls.Add(this.panel4);
+            this.panel3.Controls.Add(this.panel1);
+            this.panel3.Controls.Add(this.panel2);
+            this.panel3.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel3.Location = new System.Drawing.Point(10, 10);
+            this.panel3.Name = "panel3";
+            this.panel3.Padding = new System.Windows.Forms.Padding(5);
+            this.panel3.Size = new System.Drawing.Size(1279, 792);
+            this.panel3.TabIndex = 14;
+            // 
+            // panel4
+            // 
+            this.panel4.Controls.Add(this.dgData);
+            this.panel4.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel4.Location = new System.Drawing.Point(5, 47);
+            this.panel4.Name = "panel4";
+            this.panel4.Size = new System.Drawing.Size(1269, 710);
+            this.panel4.TabIndex = 14;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1299, 812);
+            this.Controls.Add(this.panel3);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Padding = new System.Windows.Forms.Padding(10);
+            this.Text = "Open Enrollment Client Finder Thingy Special Edition 5.6.7 (unregistered)";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgData)).EndInit();
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            this.panel3.ResumeLayout(false);
+            this.panel4.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnName;
+        private System.Windows.Forms.ComboBox cbSchools;
+        private System.Windows.Forms.Label lblSchools;
+        private System.Windows.Forms.TextBox tbEmail;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox tbPhone;
+        private System.Windows.Forms.Label lblEmail;
+        private System.Windows.Forms.Label lblfName;
+        private System.Windows.Forms.TextBox tbfName;
+        private System.Windows.Forms.Label lbllName;
+        private System.Windows.Forms.TextBox tblName;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label lblRecordCount;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.DataGridView dgData;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.Panel panel4;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox tbLast4;
+        private System.Windows.Forms.Label lblLast4;
+    }
+}
+
---
+++ KronosTesting/Form1.cs
@@ -0,0 +1,275 @@
+﻿using Newtonsoft.Json.Linq;
+using System;
+using System.Data;
+using System.Data.SqlClient;
+using System.Net.Http;
+using System.Windows.Forms;
+using unirest_net.http;
+
+namespace KronosTesting
+{
+    /// <summary>
+    /// One window for this program
+    /// </summary>
+    public partial class Form1 : Form
+    {
+        private static DataTable dtCustomers = null;
+        private static SqlConnection conn = null;
+        private static string token = ReadToken();
+        private string sqlBase = "SELECT [ID], [First Name], [Last Name], Email, [Secondary Email], [Work Phone],[Cell Phone], " +
+            "[Home Phone], [Company: Phone], Company, LEFT(CONVERT(VARCHAR, [DOB], 120), 10) AS BirthDay, " + 
+            "CONVERT(int,ROUND(DATEDIFF(hour,[DOB],GETDATE())/8766.0,0)) AS Age ,'$' + CONVERT(varchar(12), salary, 1) as Salary, " + 
+            "link, REPLICATE('0',4-LEN(RTRIM(Last4))) + RTRIM(Last4) as Last4SSN FROM All_Employees";
+        private string sql = "";
+        private static readonly HttpClient client = new HttpClient();
+        
+        private static string userName = GetUserName();
+
+        /// <summary>
+        /// Constuctor
+        /// </summary>
+        public Form1()
+        {
+            InitializeComponent();
+            StartUp();//builds the SQL connection, etc
+            WindowState = FormWindowState.Maximized;
+            Console.WriteLine(userName);
+        }
+
+
+        private static string GetUserName() {
+            return Environment.UserName;
+            //return (string)collection.Cast<ManagementBaseObject>().First()["UserName"];
+        }
+
+        /// <summary>
+        /// This will apply an sql like filter on the DataGridView according to the TextBoxes
+        /// </summary>
+        private void ResetData()
+        {
+            //build the row filter
+            sql = "([Email] like '%" + tbEmail.Text.Trim() + "%'";
+            sql += " OR [Secondary Email] like '%" + tbEmail.Text.Trim() + "%')";
+            sql += " AND ([Work Phone] like '%" + tbPhone.Text.Trim() + "%'";
+            sql += " OR [Cell Phone] like '%" + tbPhone.Text.Trim() + "%'";
+            sql += " OR [Home Phone] like '%" + tbPhone.Text.Trim() + "%')";
+
+            //NOTE: All should be converted to open wildcard
+            if(cbSchools.Text.Trim() == "All")
+            {
+                sql += " AND [Company] like '%'";
+            } else
+            {
+                sql += " AND [Company] like '%" + cbSchools.Text.Trim() + "%'";
+            }
+
+            sql += " AND [First Name] like '%" + tbfName.Text.Trim() + "%'";
+            sql += " AND [Last Name] like '%" + tblName.Text.Trim() + "%'";
+
+            //Adding last 4
+
+            sql += " AND [Last4SSN] like'%" + tbLast4.Text.Trim() + "%'";
+            Console.WriteLine(sql);
+            
+            //set row filter
+            ((DataTable)dgData.DataSource).DefaultView.RowFilter = sql;
+
+            //update record count
+            lblRecordCount.Text = "Record Count: " + ((DataTable)dgData.DataSource).DefaultView.Count;
+        }
+
+        /// <summary>
+        /// Builds SQL connection, loads data to DataGridView, 
+        /// </summary>
+        private void StartUp()
+        {
+            dgData.AutoSize = true;
+            dgData.Dock = DockStyle.Fill;
+            dtCustomers = new DataTable();
+            //BuildSQL();
+            Console.WriteLine(sqlBase);
+            conn = new SqlConnection("Data Source=RALIMSQL1;Initial Catalog=Kronos;Integrated Security=SSPI;");
+            SqlCommand cmd = new SqlCommand(sqlBase, conn);
+            conn.Open();
+            SqlDataAdapter adapter = new SqlDataAdapter(cmd);
+            adapter.Fill(dtCustomers);
+            dgData.DataSource = dtCustomers;
+
+            lblRecordCount.Text = "Record Count: " + ((DataTable)dgData.DataSource).DefaultView.Count;
+            dgData.Refresh();
+
+            cmd = new SqlCommand("SELECT Company FROM [Kronos].[dbo].[All_Employees] GROUP BY Company", conn);
+            cmd.CommandType = System.Data.CommandType.Text;
+            SqlDataReader reader = cmd.ExecuteReader();
+            cbSchools.Items.Clear();
+            cbSchools.Items.Add("All");
+            while (reader.Read())
+            {
+                cbSchools.Items.Add(reader.GetString(0));
+            }
+            conn.Close();
+            cmd.Dispose();
+            cbSchools.SelectedIndex = 0;
+            conn.Close();
+            ((DataTable)dgData.DataSource).DefaultView.RowFilter = null;
+            lblRecordCount.Text = "Record Count: " + ((DataTable)dgData.DataSource).DefaultView.Count;
+            //hide the ID column
+            dgData.Columns[0].Visible = false;
+        }
+
+        private void btnName_Click(object sender, EventArgs e)
+        {
+            ((DataTable)dgData.DataSource).DefaultView.RowFilter = null;
+            lblRecordCount.Text = "Record Count: " + ((DataTable)dgData.DataSource).DefaultView.Count;
+            tbEmail.Text = "";
+            tbfName.Text = "";
+            tblName.Text = "";
+            tbPhone.Text = "";
+            tbLast4.Text = "";
+        }
+
+
+        private void cbSchools_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            ResetData();
+        }
+
+        private void tbEmail_TextChanged(object sender, EventArgs e)
+        {
+            ResetData();
+        }
+
+        private void tbPhone_TextChanged(object sender, EventArgs e)
+        {
+            ResetData();
+        }
+
+        private void tblName_TextChanged(object sender, EventArgs e)
+        {
+            ResetData();
+        }
+
+        private void tbfName_TextChanged(object sender, EventArgs e)
+        {
+            ResetData();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            btnName_Click(null, null);
+            StartUp();
+        }
+
+        private async void button2_Click(object sender, EventArgs e)
+        {
+            DataGridViewRow selectedRow = null;
+            if(dgData.SelectedRows.Count == 0) {
+                int selected = dgData.SelectedCells[0].RowIndex;
+                selectedRow = dgData.Rows[selected]; 
+            } else {
+                selectedRow = dgData.SelectedRows[0];
+            }
+
+            string postData = BuildJson(selectedRow);
+            string userID = selectedRow.Cells[0].Value.ToString();
+            //Console.WriteLine(postData);
+
+            HttpResponse<string> jsonResponse = Unirest.post("https://api.getbase.com/v2/leads")
+                  .header("accept", "application/json")
+                  .header("Authorization", "Bearer " + token)
+                  .header("Content-Type", "application/json")
+                  .body(postData)
+                  .asJson<string>();
+
+            string body = jsonResponse.Body.ToString();
+            JObject jo = JObject.Parse(body);
+            string leadID = jo["data"]["id"].ToString();
+            string URL = "https://app.futuresimple.com/leads/" + leadID;
+
+            if(MessageBox.Show("Would you like to open the lead in base?","Open in Base?", MessageBoxButtons.YesNo) == DialogResult.Yes){
+                System.Diagnostics.Process.Start(URL);
+            }
+
+            bool updated = WriteLink(URL, userID);
+            if (updated) {
+                selectedRow.Cells[13].Value = URL;
+            }
+
+            Console.WriteLine(jo);
+        }
+
+        private static bool WriteLink(string link, string userID) {
+            SqlConnection tconn = new SqlConnection("Data Source=RALIMSQL1;Initial Catalog=Kronos;Integrated Security=SSPI;");
+            string sqlStr = "UPDATE [Kronos].[dbo].[All_Employees] SET[link] = '" + link + "' WHERE[ID] = '" + userID + "'";
+
+            SqlCommand cmd = new SqlCommand(sqlStr, tconn);
+            tconn.Open();
+            cmd.CommandType = System.Data.CommandType.Text;
+            int rAffected = cmd.ExecuteNonQuery();
+            conn.Close();
+            cmd.Dispose();
+            if (rAffected != 1) {
+                MessageBox.Show("Something went wrong writing link the database\n The lead was created however.");
+                return false;
+            } else {
+                return true;
+            }
+        }
+
+        private static string GetUserID() {
+            string id = "";
+            SqlConnection tconn = new SqlConnection("Data Source=RALIMSQL1;Initial Catalog=Kronos;Integrated Security=SSPI;");
+            SqlCommand cmd = new SqlCommand("SELECT Kronos.dbo.userIDs.id FROM[Kronos].[dbo].[userIDs] Where uname = '" + userName +"';", tconn);
+            tconn.Open();
+            cmd.CommandType = System.Data.CommandType.Text;
+            SqlDataReader reader = cmd.ExecuteReader();
+            while (reader.Read()) {
+               id = reader.GetString(0);
+            }
+            conn.Close();
+            cmd.Dispose();
+            return id;
+        }
+
+        private void tbLast4_TextChanged(object sender, EventArgs e) {
+            ResetData();
+        }
+
+        public static string BuildJson(DataGridViewRow row) {
+            string[] arr = new string[1];
+            arr[0] = "2017OE";
+
+            JObject custSub = new JObject(
+                new JProperty("Worksite Email", row.Cells[4].Value),
+                new JProperty("Worksite Phone", row.Cells[5].Value),
+                new JProperty("Home Phone", row.Cells[7].Value),
+                new JProperty("District Phone", row.Cells[8].Value),
+                new JProperty("District", row.Cells[9].Value),
+                new JProperty("DOB", row.Cells[10].Value),
+                new JProperty("Annual Salary", row.Cells[12].Value)
+                );
+            JObject data = new JObject(
+                        new JProperty("owner_id", Convert.ToInt32(GetUserID())),
+                        new JProperty("first_name", row.Cells[1].Value),
+                        new JProperty("last_name", row.Cells[2].Value),
+                        new JProperty("email", row.Cells[3].Value),
+                        new JProperty("mobile", row.Cells[6].Value),
+                        new JProperty("tags", new JArray(arr)),
+                        new JProperty("custom_fields", custSub)
+                        );
+
+            JObject json = new JObject(new JProperty("data", data));
+            return json.ToString();
+        }
+
+        private static string ReadToken() {
+            try {
+                System.IO.StreamReader tFile = new System.IO.StreamReader("\\\\nas3\\Shared\\RALIM\\James\\token.tkn");
+                return tFile.ReadToEnd();
+            } catch (Exception e) {
+                MessageBox.Show("Could not find Base Token\nUnable to push lead\nContact Admin");
+                return "";
+            }
+        }
+    }
+}
